c34d61587dbf5a7a85017a9924a9aec4be5d069c
Harshak777 <harshakkrishnaa777@gmail.com>
2024-03-31 19:56:07 -0700

    Created an initial ProductService to save Product

---
+++ product-service/src/main/java/com/sbmicroservies/productservice/service/ProductService.java
@@ -0,0 +1,25 @@
+package com.sbmicroservies.productservice.service;
+
+import org.springframework.stereotype.Service;
+
+import com.sbmicroservies.productservice.dto.ProductRequest;
+import com.sbmicroservies.productservice.model.Product;
+import com.sbmicroservies.productservice.repository.ProductRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+	private final ProductRepository productRepository;
+	
+	public void createProduct(ProductRequest productRequest) {
+		Product product = Product.builder()
+					.name(productRequest.getName())
+					.description(productRequest.getDescription())
+					.price(productRequest.getPrice())
+					.build();
+		
+		productRepository.save(product);
+	}
+}
