3bd250941feac7b33f02b440434813169aafeb60
Harshak777 <harshakkrishnaa777@gmail.com>
2024-03-31 21:20:38 -0700

    Initial test setup

--- product-service/src/test/java/com/sbmicroservies/productservice/ProductServiceApplicationTests.java
+++ product-service/src/test/java/com/sbmicroservies/productservice/ProductServiceApplicationTests.java
@@ -2,10 +2,24 @@ package com.sbmicroservies.productservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
 class ProductServiceApplicationTests {
 
+	@Container
+	static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo: 4.4.2");
+	
+	@DynamicPropertySource
+	static void setProperties(DynamicPropertyRegistry dymDynamicPropertyRegistry) {
+		dymDynamicPropertyRegistry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
+	}
+	
 	@Test
 	void contextLoads() {
 	}
