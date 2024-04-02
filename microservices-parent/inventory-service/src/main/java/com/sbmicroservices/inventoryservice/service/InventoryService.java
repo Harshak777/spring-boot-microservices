ab7f50e78588d6c1e112938a00409b1e7e5e4fce
Harshak777 <harshakkrishnaa777@gmail.com>
2024-04-02 13:24:22 -0700

    Implemented InventoryService

---
+++ microservices-parent/inventory-service/src/main/java/com/sbmicroservices/inventoryservice/service/InventoryService.java
@@ -0,0 +1,20 @@
+package com.sbmicroservices.inventoryservice.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.sbmicroservices.inventoryservice.repository.InventoryRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class InventoryService {
+	
+	private final InventoryRepository inventoryRepository;
+
+	@Transactional(readOnly = true)
+	public boolean isInStock(String skuCode) {
+		return inventoryRepository.findBySkuCode().isPresent();
+	}
+}
