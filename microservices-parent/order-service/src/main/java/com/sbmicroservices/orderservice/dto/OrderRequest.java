ce6bfe50389ea7ddd5f99fe40d4552ed58ec3751
Harshak777 <harshakkrishnaa777@gmail.com>
2024-04-01 17:49:25 -0700

    Fixed import error in OrderRequest DTO

--- microservices-parent/order-service/src/main/java/com/sbmicroservices/orderservice/dto/OrderRequest.java
+++ microservices-parent/order-service/src/main/java/com/sbmicroservices/orderservice/dto/OrderRequest.java
@@ -1,5 +1,11 @@
 package com.sbmicroservices.orderservice.dto;
 
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
