0e4b20d861e9a86d4a8ad56ee3685fd27cebbbaa
Harshak Krishnaa <33751325+Harshak777@users.noreply.github.com>
2024-04-02 23:10:41 -0700

    Delete microservices-parent directory

--- microservices-parent/order-service/src/main/java/com/sbmicroservices/orderservice/repository/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sbmicroservices.orderservice.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.sbmicroservices.orderservice.model.Order;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-
-}
--- microservices-parent/product-service/src/main/java/com/sbmicroservies/productservice/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sbmicroservies.productservice.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.sbmicroservies.productservice.model.Product;
-
-public interface ProductRepository extends MongoRepository<Product, String> {
-
-}
