bf5cf2e62aa1b308e4b1f1efcbaaa1f5a5298e44
Harshak777 <harshakkrishnaa777@gmail.com>
2024-04-07 12:02:21 -0700

    Configured Client Server to get DS registry

--- microservices/inventory-service/src/main/java/com/sbmicroservices/inventoryservice/InventoryServiceApplication.java
+++ microservices/inventory-service/src/main/java/com/sbmicroservices/inventoryservice/InventoryServiceApplication.java
@@ -3,12 +3,14 @@ package com.sbmicroservices.inventoryservice;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 import com.sbmicroservices.inventoryservice.model.Inventory;
 import com.sbmicroservices.inventoryservice.repository.InventoryRepository;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class InventoryServiceApplication {
 
 	public static void main(String[] args) {
